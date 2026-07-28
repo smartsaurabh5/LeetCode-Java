46561508188449c30856231aca38d06b319b14ce
Saurabh Pal <132087596+smartsaurabh5@users.noreply.github.com>
2026-07-29 02:10:32 +0530

    Time: 16 ms (93.07%), Space: 45.9 MB (14.18%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.java
+++ 0016-3sum-closest/0016-3sum-closest.java
@@ -1,33 +1,34 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
+    int n= nums.length;
+    Arrays.sort(nums);///sort krna jarruri haii;
+    int closet = nums[0]+nums[1]+nums[2];
 
-        Arrays.sort(nums);
+    for(int i =0;i<n-2;i++){////confusion ek hoga jaise 3 vaule chahiye toh i ki value n-2 se jyada chlane ka koi mtlb ni bnegea kyuki do value hi nhi bachegi aaaur 3 sum aaur 4 sum ke concept me while use tha same value na use ho baar baar ..kyuki triplet alg chahiye the wha pe indices se koi mtlb ni tha ...value different chahiye the ..but yha pe vlaue same chalegi bs indices(index) different honi chhaiye ;
+        int j =i+1;
+        int k=n-1;
 
-        int closest = nums[0] + nums[1] + nums[2];
+        while(j<k){
+            int sum = nums[i]+nums[j]+nums[k];
+            if(Math.abs(sum-target)<Math.abs(closet-target)){//check kr rhe kaunsa jyada pass haii abs means absolute jo choti h mtlb jyada pass haii to closet me daal diya hu ;
+                closet =sum;
+            }
 
-        for (int i = 0; i < nums.length - 2; i++) {
+            if(sum<target){//sum agr target se chota haii toh j++ same whi 3 sum 4 sum wala concept 
+               j++;
+            }
 
-            int left = i + 1;
-            int right = nums.length - 1;
+            else if(sum>target){///bda haii toh k-- me kr do...
+                k--;
+            }
 
-            while (left < right) {
-
-                int sum = nums[i] + nums[left] + nums[right];
-
-                if (Math.abs(sum - target) < Math.abs(closest - target)) {
-                    closest = sum;
-                }
-
-                if (sum < target) {
-                    left++;
-                } else if (sum > target) {
-                    right--;
-                } else {
-                    return target;
-                }
+            else{
+                return sum; //agr sum hi mil gya toh return kr denge sidha sum;
             }
         }
+    }
 
-        return closest;
+   return closet;///akhiri me closet wali value ko return kr dunga,...
+
     }
 }

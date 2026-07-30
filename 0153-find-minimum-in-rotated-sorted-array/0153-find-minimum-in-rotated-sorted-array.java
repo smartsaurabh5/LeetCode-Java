a2ee97d4fa5bc6939dee8610a1e7fb87239f9e76
Saurabh Pal <132087596+smartsaurabh5@users.noreply.github.com>
2026-07-30 20:21:57 +0530

    Time: 0 ms (100%), Space: 43.5 MB (94.89%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,10 +1,18 @@
 class Solution {
     public int findMin(int[] nums) {
-        for(int i =1;i<nums.length;i++){
-          if(nums[i]<nums[i-1]){
-            return nums[i];
-          }
+      int left =0;
+      int right = nums.length-1;
+
+      while(left<right){
+        int mid = left+(right-left)/2;
+        if(nums[mid]>nums[right]){
+            //iska mtlb h ki minimum aabhi right side me hi lie kr rha haii to kyuki array rotated and sorted haaiii;to left ko mid +1; kr denge aaur agr chotaa hua right se mtlb ki left me hi lie kr rha ....to right ko mid ke brabar ke denge kyuki agr chota haii mtlb kya pta whin number sbse chota ho isliye whi se start ke rhe aab dekhna h ussse chota kaunsa h ;
+            left = mid+1;
         }
-        return nums[0];
+        else{
+            right =mid;
+        }
+      }
+      return nums[left];//jarruri nhi h ki left hi waps kre ...koi sa bhi kr skte h kyuki at the end dono left and right ek ko hi point karenge ;
     }
 }

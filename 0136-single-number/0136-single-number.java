84efe959853bedc26b4d572bf17d3dca65b70dfd
Saurabh Pal <132087596+smartsaurabh5@users.noreply.github.com>
2026-07-30 18:45:58 +0530

    Time: 1 ms (99.96%), Space: 46.8 MB (65.36%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,11 +1,9 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int ans = 0;
-
-        for (int num : nums) {
-            ans ^= num;
-        }
-
-        return ans;
+     int ans=0;
+     for(int i =0;i<nums.length;i++){
+        ans=ans^nums[i];
+     }
+     return ans;
     }
-}
+}//tughe concept smjhata hu xor ke pass associate property aaur commutative property hoti toh aaru tu janta h same value ka xor krne pe 0 aa jata h ...to ye mt soch ki yha fir sum krte ja rhe kya ki akhiri me sbka zero ho gya aaur single bch gya ..aaisha nhi h..yha pe kya kr rhe ki associative property toh maanle ye h  a^b^c^a^b^c ....toh iska answer aana zero hi haii ...chahe a^b krke fir ^a lo toh bhi aaiyega ya fir a^a krke ^b lo to bhi b hi aaiyegi means vo adjust automatically ho jaiyega aagr sbb even digit times aaye h aaaur koi number bs ek baar aaya h simple...(xor ki commutataive aaaur asssociative property yaad kr lena smjh me aa jaiyega)

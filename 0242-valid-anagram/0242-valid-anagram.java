515cdc8230f60c3da68ea9d745cd29607c05d856
Saurabh Pal <132087596+smartsaurabh5@users.noreply.github.com>
2026-07-31 22:06:07 +0530

    Time: 6 ms (43.29%), Space: 44.5 MB (66.96%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,18 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-     char[] arr= s.toCharArray();
-     char[] arr1 =t.toCharArray();
-     Arrays.sort(arr);
-     Arrays.sort(arr1);
-     String sorted = new String(arr);
-     String sorted1 = new String(arr1);
-    return sorted.equals(sorted1);
+     int[] arr=new int[26];
 
+     if(s.length()!=t.length())return false;
+
+     for(int i =0;i<s.length();i++){
+        arr[s.charAt(i)-'a']++;
+        arr[t.charAt(i)-'a']--;
+     }
+     for(int count:arr){
+        if(count!=0){
+            return false;
+        }
+     }
+     return true;
     }
-}///this is basically brute force we cant able to  use sort in string so firstly need to covert into char array and then sort and again convert it into string to use .euqals because cant use in array ...the retunr ye if both string equals otherwise retunr false;
+}//isme kya kr rhe ...ek arr le le rhe 26 space ki kyuki alphabet 26 ki hoti haii ..anagram hone ke liye toh dono ka length brabar hona hi chahiye nhi toh khel whi khtm okh...uske baad ek loop liya usme kya kr rhe kisi bhi ek string ki length tk chalega uske..dono ki lenght brabr h kyuki uske baad main concept ye haii ki ...small start hota h 97 se ..toh pehli strin gke pehle character ko leke a me se ghta de rhe toh jo alphabet me uski position hoti h vo  mil ja rhi ...aaur uspe jake ++ kr de rhe ..then dusri wali string me aagr same word hoga to -- ho jaiyega ...to agr ek bhi alg mil toh hr 26 ki 26 jgh arr me 0 nhi hogaa to fir ek loop chla liya aagr khi bhi o ke alawa kuch mila retunr false otherwise return true;

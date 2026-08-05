be5de5f10e58df5feb0747f77f75e1f1fd7d7e24
Saurabh Pal <132087596+smartsaurabh5@users.noreply.github.com>
2026-08-06 00:06:55 +0530

    Time: 71 ms (58.65%), Space: 289.9 MB (45.19%) - LeetHub

---
+++ 3310-remove-methods-from-project/3310-remove-methods-from-project.java
@@ -0,0 +1,50 @@
+class Solution {
+    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
+
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : invocations) {
+            graph.get(edge[0]).add(edge[1]);
+        }
+
+        boolean[] suspicious = new boolean[n];
+
+        dfs(k, graph, suspicious);
+
+        // Check if any non-suspicious method calls a suspicious method
+        for (int[] edge : invocations) {
+            int u = edge[0];
+            int v = edge[1];
+
+            if (!suspicious[u] && suspicious[v]) {
+                List<Integer> ans = new ArrayList<>();
+                for (int i = 0; i < n; i++) {
+                    ans.add(i);
+                }
+                return ans;
+            }
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            if (!suspicious[i]) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
+    private void dfs(int node, List<List<Integer>> graph, boolean[] suspicious) {
+        suspicious[node] = true;
+
+        for (int next : graph.get(node)) {
+            if (!suspicious[next]) {
+                dfs(next, graph, suspicious);
+            }
+        }
+    }
+}
